9127e58fde9f939da5b2849adb74bdd65ad4eb48
ItsMariusBC <marius.bc@ik.me>
2026-02-22 02:24:15 +0100

    chore: migrate framer-motion → motion v12

--- components/magicui/dock.tsx
+++ components/magicui/dock.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { motion, useMotionValue, useSpring, useTransform } from "framer-motion";
+import { motion, useMotionValue, useSpring, useTransform } from "motion/react";
 
 export interface DockContextType {
   hovered: boolean;
