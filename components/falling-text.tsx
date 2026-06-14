3fcd4b98c700d1014e59b1dc66ba596661f6853a
ItsMariusBC <marius.bc@ik.me>
2026-06-14 23:26:06 +0200

    fix(falling-text): on-DA highlight (bone bold+underline, drop cyan)

--- components/falling-text.tsx
+++ components/falling-text.tsx
@@ -36,7 +36,7 @@ const FallingText: React.FC<FallingTextProps> = ({
       .map(word => {
         const isHighlighted = highlightWords.some(hw => word.startsWith(hw));
         return `<span
-          class="inline-block mx-[2px] select-none ${isHighlighted ? 'text-cyan-500 font-bold' : ''}"
+          class="inline-block mx-[2px] select-none ${isHighlighted ? 'font-black underline decoration-bone/50 underline-offset-4' : ''}"
         >
           ${word}
         </span>`;
